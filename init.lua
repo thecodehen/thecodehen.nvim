5d379f7617343db0dbee501649bd7525c9483e44
thecodehen <6582598+thecodehen@users.noreply.github.com>
2025-07-25 00:44:15 +0200

    Add formatter and linter for Python

--- init.lua
+++ init.lua
@@ -717,6 +717,8 @@ require('lazy').setup({
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
+        'black', -- Used to format Python code
+        'ruff', -- Used to lint Python code
         'stylua', -- Used to format Lua code
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
@@ -770,6 +772,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        python = { 'black' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
