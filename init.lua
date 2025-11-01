32fc4d0e17c9c9a114df18c7d8e73ec660169500
thecodehen <6582598+thecodehen@users.noreply.github.com>
2025-11-01 12:34:47 +0100

    Add REPL

--- init.lua
+++ init.lua
@@ -174,6 +174,9 @@ vim.o.number = true
 vim.o.relativenumber = true
 vim.o.signcolumn = 'number'
 
+-- Indention
+vim.o.expandtab = true
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
